0ada507638455de139d4c3e0fed9fd6db80654b1
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-06-03 13:16:33 -0300

    actualizado jwtUtils

--- sb-ecom/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ sb-ecom/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -74,6 +74,7 @@ public class JwtUtils {
                 .path("/api")
                 .maxAge(24*60*60)//duracion x 1 dia
                 .httpOnly(false)
+                .secure(false)
                 .build();
 
         return cookie;
