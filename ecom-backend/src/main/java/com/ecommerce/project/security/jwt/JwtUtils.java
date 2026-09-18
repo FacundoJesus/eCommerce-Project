16a8ab11c0289ab6286e5871b278f9cfd377eb39
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-09-18 14:39:27 -0300

    fix: update JWT cookie for cross-domain usage (SameSite=None, Secure=true)

--- ecom-backend/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ ecom-backend/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -72,8 +72,8 @@ public class JwtUtils {
                 .path("/api")
                 .maxAge(24 * 60 * 60)// duracion x 1 dia
                 .httpOnly(true)
-                .secure(false)
-                .sameSite("Lax")
+                .secure(true)
+                .sameSite("None")
                 .build();
 
         return cookie;
@@ -83,6 +83,10 @@ public class JwtUtils {
     public ResponseCookie getCleanJwtCookie() {
         ResponseCookie cookie = ResponseCookie.from(jwtCookie, null)
                 .path("/api")
+                .maxAge(0)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
                 .build();
         return cookie;
     }
